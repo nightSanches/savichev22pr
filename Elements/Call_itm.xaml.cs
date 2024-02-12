6014008112ff71c028c01cd45ab7b1e4da29b4a8
gsavichev <galesavich@gmail.com>
2024-02-13 01:22:22 +0500

    Разработанная страница Call_itm

---
+++ Elements/Call_itm.xaml.cs
@@ -0,0 +1,96 @@
+﻿using ClassModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace savichev22pr.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Call_itm.xaml
+    /// </summary>
+    public partial class Call_itm : UserControl
+    {
+        Call call_loc;
+        public Call_itm(Call _call)
+        {
+            InitializeComponent();
+            call_loc = _call;
+            if (_call.time_end != null)
+            {
+                User user_loc = MainWindow.connect.users.Find(x => x.id == _call.user_id);
+                category_call_text.Content = user_loc.fio_user.ToString();
+
+                string[] dateloc1 = _call.time_start.ToString().Split(' ');
+                string[] dateloc2 = _call.time_end.ToString().Split(' ');
+
+                string[] date1 = (dateloc1[0]).Split('.');
+                string[] date2 = (dateloc2[0]).Split('.');
+
+                System.DateTime dateStart = new DateTime(int.Parse(date1[2]),
+                    int.Parse(date1[1]),
+                    int.Parse(date1[0]),
+                    int.Parse(dateloc1[1].Split(':')[0]),
+                    int.Parse(dateloc1[1].Split(':')[1]), 0);
+
+                System.DateTime dateFinish = new DateTime(int.Parse(date2[2]),
+                    int.Parse(date2[1]),
+                    int.Parse(date2[0]),
+                    int.Parse(dateloc2[1].Split(':')[0]),
+                    int.Parse(dateloc2[1].Split(':')[1]), 0);
+                System.TimeSpan dateEnd = dateFinish.Subtract(dateStart);
+
+                time_call_text.Content = "Продолжительность звонка:" + dateEnd.ToString();
+                number_call_text.Content = "Номер телефона: " + user_loc.phone_num.ToString();
+            }
+            img_category_call.Source =
+                (_call.category_call == 1) ?
+                new BitmapImage(new Uri(@"/img/out.png", UriKind.RelativeOrAbsolute)) : new BitmapImage(new Uri(@"/img/in.png", UriKind.RelativeOrAbsolute));
+
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 0;
+            opgridAnimation.To = 1;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.4);
+            border.BeginAnimation(StackPanel.OpacityProperty, opgridAnimation);
+        }
+
+        private void Click_redact(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Anim_move(MainWindow.main.scroll_main, MainWindow.main.frame_main, MainWindow.main.frame_main, new Pages.PagesUser.Call_win(call_loc));
+        }
+
+
+        private void Click_remove(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainWindow.connect.LoadData(ClassConnection.Connection.tabels.calls);
+
+                string vs = "DELETE FROM [calls] WHERE [Код] = " + call_loc.id.ToString() + "";
+                var pc = MainWindow.connect.QueryAccess(vs);
+                if (pc != null)
+                {
+                    MessageBox.Show("Успешное удаление звонка", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MainWindow.connect.LoadData(ClassConnection.Connection.tabels.calls);
+                    MainWindow.main.Anim_move(MainWindow.main.frame_main, MainWindow.main.scroll_main, null, null, Pages.Main.page_main.calls);
+                }
+                else MessageBox.Show("Запрос на удаление не был обработан", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
