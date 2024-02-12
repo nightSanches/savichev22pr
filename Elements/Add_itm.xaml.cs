ea07326554766ea633aa3b9cedb656fa9ef415ad
gsavichev <galesavich@gmail.com>
2024-02-13 01:26:03 +0500

    Разработанная страница Add_itm

---
+++ Elements/Add_itm.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace savichev22pr.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_itm.xaml
+    /// </summary>
+    public partial class Add_itm : UserControl
+    {
+
+        Page page_str;
+        public Add_itm(Page _page_str)
+        {
+            InitializeComponent();
+            page_str = _page_str;
+
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 0;
+            opgridAnimation.To = 1;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.4);
+            border.BeginAnimation(StackPanel.OpacityProperty, opgridAnimation);
+        }
+
+        private void Click_add(object sender, RoutedEventArgs e)
+        {
+            MainWindow.main.Anim_move(MainWindow.main.scroll_main, MainWindow.main.frame_main, MainWindow.main.frame_main, page_str);
+        }
+    }
+}
